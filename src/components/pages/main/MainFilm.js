93668593e4d2bcfa86e89ce7b39ed54917ae3b33
Aijjou <mohamed.aijjou1780@gmail.com>
2020-07-09 19:06:53 +0200

    test

--- src/components/pages/main/MainFilm.js
+++ src/components/pages/main/MainFilm.js
@@ -1,28 +1,19 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
-import { Arrow } from "./Arrow";
-import { Slide } from "./Slide";
-import { Indicator } from "./Indicator";
-import "./Main.css";
-=======
->>>>>>> 7a250d617900b81c5bf040a0ac43c5e9bb62ece4
+
 import riverdale from "./../riverdale.png";
 import casapapel from "./../casa-papel.jpg";
 import kingdom from "./../the-last-kingdom.jpg";
 import who from "./../doctor-who.jpg";
-<<<<<<< HEAD
 
-=======
 import "./Main.css";
-import { Arrow } from "./Arrow"
-import { Slide } from "./Slide"
-import { Indicator } from "./Indicator"
->>>>>>> 7a250d617900b81c5bf040a0ac43c5e9bb62ece4
+import { Arrow } from "./Arrow";
+import { Slide } from "./Slide";
+import { Indicator } from "./Indicator";
+
 export class MainFilm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-<<<<<<< HEAD
       slides: [
         {
           titre: "Riverdale",
@@ -49,56 +40,10 @@ export class MainFilm extends Component {
           image: who
         }
       ],
-
       indexActivation: undefined
     };
   }
-  componentDidMount() {
-    this.setState({
-      indexActivation: this.state.slides.length - 1
-    });
-=======
-      slides: [{
-        titre: "Riverdale",
-        age: "+16 ans",
-        categorie: "Sentimentale",
-        image: riverdale
-      },
-      {
-        titre: "La Casa Del Papel",
-        age: "+16 ans",
-        categorie: "Thriller",
-        image: casapapel
-      },
-      {
-        titre: "Kingdom",
-        age: "+16 ans",
-        categorie: "Aventure",
-        image: kingdom
-      },
-      {
-        titre: "Doctor Who",
-        age: "+16 ans",
-        categorie: "Sc-Fi",
-        image: who
-      }
-    ],
-      indexActivation: undefined
-    };
->>>>>>> 7a250d617900b81c5bf040a0ac43c5e9bb62ece4
-  }
-  leftClick = () => {
-    let tmpIndex = this.state.indexActivation - 1;
-    if (this.state.indexActivation == 0) {
-      tmpIndex = this.state.slides.length - 1;
-    }
-    this.setState({
-      indexActivation: tmpIndex
-    });
-  };
 
-<<<<<<< HEAD
-=======
   componentDidMount() {
     this.setState({
       indexActivation: this.state.slides.length - 1
@@ -114,7 +59,21 @@ export class MainFilm extends Component {
     });
   };
 
->>>>>>> 7a250d617900b81c5bf040a0ac43c5e9bb62ece4
+  componentDidMount() {
+    this.setState({
+      indexActivation: this.state.slides.length - 1
+    });
+  }
+  leftClick = () => {
+    let tmpIndex = this.state.indexActivation - 1;
+    if (this.state.indexActivation == 0) {
+      tmpIndex = this.state.slides.length - 1;
+    }
+    this.setState({
+      indexActivation: tmpIndex
+    });
+  };
+
   rightClick = () => {
     let tmpIndex = this.state.indexActivation + 1;
     if (this.state.indexActivation == this.state.slides.length - 1) {
@@ -133,10 +92,7 @@ export class MainFilm extends Component {
             return (
               <Slide
                 index={i}
-<<<<<<< HEAD
-=======
                 titre={s.titre}
->>>>>>> 7a250d617900b81c5bf040a0ac43c5e9bb62ece4
                 image={s.image}
                 indexActivation={this.state.indexActivation}
               />
