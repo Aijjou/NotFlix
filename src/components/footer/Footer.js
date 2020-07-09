78dbe047d3c34e33161ce0c21d3c9a329c6d0c33
Alexandre Roy <alexandreroy01@gmail.com>
2020-07-09 15:17:58 +0200

    Footer done 09/07 15h20

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -12,21 +12,21 @@ export class Footer extends Component {
     return (
       <div>
          <footer className="footer-container" id="footer">
-        {/* <div className="row bd-highlight mb-3"> */}
+        <div className="row bd-highlight mb-3">
             <div className="btn mr-auto p-2 bd-highlight">
-                <a className="btn col-1" href="https://www.facebook.com/netflixfrance" target="_blank" id="btnf" >
+                <a className="btn col-1 mr-2" href="https://www.facebook.com/netflixfrance" target="_blank" id="btnf" >
 
                     <i class="fa fa-facebook-official" aria-hidden="true" id="iconf"></i>
 
                 </a>
 
-                <a className="btn col-1 " href="https://www.instagram.com/netflixfr/?hl=fr" target="_blank" id="btni">
+                <a className="btn col-1 mr-2 " href="https://www.instagram.com/netflixfr/?hl=fr" target="_blank" id="btni">
 
                     <i class="fa fa-instagram" aria-hidden="true" id="iconi"></i>
 
                 </a>
 
-                <a className="btn col-1" href="https://twitter.com/NetflixFR" target= "_blank" id="btnt">
+                <a className="btn col-1 mr-2" href="https://twitter.com/NetflixFR" target= "_blank" id="btnt">
 
                     <i class="fa fa-twitter-square" aria-hidden="true" id="icont"></i>
 
@@ -36,7 +36,7 @@ export class Footer extends Component {
                 <p className="text-muted col-12" href="#" id="txt"> © 2020-2020 Notflix, Inc.</p>
 
             </div>
-        {/* </div> */}
+        </div>
     </footer>
       </div>
     );
