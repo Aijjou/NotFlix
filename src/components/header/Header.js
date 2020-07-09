51aef9f9bd3991230ee64b2602a7de294fc6b7fb
Tanguy <tanguy.boeuf@gmail.com>
2020-07-09 16:11:35 +0200

    Header polished + CSS + Notflix_logo.png

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,50 @@
 import { Component } from "react";
 import React from 'react';
-import "./Header.css"
+import "./Header.css";
+import { Link } from "react-router-dom";
+import  notflix  from "./notflix_logo.png"
 
 export class Header extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
-
-  render() {
-    return <div>header</div>;
+    render() {
+      return (
+          <header id="header" className="container-fluid">
+            <div className="row m-2">
+                  <img id="logo" className="col-md-1 " src={notflix}/>
+                <div className="col-md-7 offset-md-7">
+                    <input id="search" type="text" class="form-control" className="col-md-3" placeholder="Rechercher votre série"></input>
+                  
+                    <a id="icon" href="" className="col-md-1" ><i class="fa fa-search" aria-hidden="true"></i></a>
+                    <a id="icon" href="" className="col-md-1" ><i class="fa fa-bell-o" aria-hidden="true"></i></a>
+                    <a id="icon" href="" className="col-md-1" ><i class="fa fa-user-circle" aria-hidden="true"></i></a>
+                </div>
+            </div>
+            
+              <nav id="nav" className="navbar navbar-expand-lg navbar-light">
+                  <ul className="navbar-nav mr-auto">
+                    <li  className="nav-item">
+                          <Link to='/' id="li" className="nav-link">Accueil</Link>
+                      </li>
+                       <li  className="nav-item">
+                          <Link to='/Serie' id="li" className="nav-link">Séries</Link>
+                      </li>
+                      <li className="nav-item">
+                          <Link to='/Film' id="li" className="nav-link">Films</Link>
+                      </li>
+                      <li  className="nav-item">
+                          <Link to='/News' id="li" className="nav-link">Nouveautées</Link>
+                      </li>
+                      <li className="nav-item">
+                          <Link to='/MyList' id="li" className="nav-link">Ma Liste</Link>
+                      </li>
+                  </ul>
+              </nav>
+          </header>
+      )
   }
 }
+  
+
